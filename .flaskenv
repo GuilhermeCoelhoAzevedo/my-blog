45a1ab2853eb06c2817dc3ba6f9b6d75033df4b2
Guilherme <guilherme@Admins-MacBook-Pro.local>
2022-02-24 22:14:08 +0000

    First commit

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,3 @@
 FLASK_APP=app.py
 FLASK_ENV=production
+DATABASE_URL=sqlite:///myblog.db
--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__, static_folder='frontend/my-blog/build', static_url_path='')
-basedir = os.path.abspath(os.path.dirname(__file__))
-    
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'myblog.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app) 
