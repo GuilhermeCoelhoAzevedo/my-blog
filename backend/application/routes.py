87b83b828dfe640b774d592141274199415e4a9f
Guilherme <guilherme@Admins-MacBook-Pro.local>
2022-02-24 23:52:23 +0000

    First commit

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -14,7 +14,7 @@ def json_definition(article):
 ############# 
 #React routes
 ############# 
-
+"""
 @app.route('/', methods=['GET'])
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
@@ -30,15 +30,16 @@ def articles_list():
 @app.route('/article/<string:name>', methods=['GET'])
 def article(name: str):
     return send_from_directory(app.static_folder, 'index.html')
-
 """
-@app.route('/', methods=['GET'])
-@app.route('/about', methods=['GET'])
-@app.route('/articles-list', methods=['GET'])
+
+
 @app.route('/article/<string:name>', methods=['GET'])
+@app.route('/articles-list', methods=['GET'])
+@app.route('/about', methods=['GET'])
+@app.route('/', methods=['GET'])
 def catch_all():
     return send_from_directory(app.static_folder, 'index.html')
-"""
+
 ############# 
 #API routes
 ############# 
